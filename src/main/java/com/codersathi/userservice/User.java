d9b60503a9e644209d57b8eefc6bb4a7d0913da3
Yuba Raj Kalathoki <yubaraj.kalathoki@zurelsoft.com>
2022-11-08 19:52:37 +0545

    CRUD implementation with mysql atabase

--- src/main/java/com/codersathi/userservice/User.java
+++ src/main/java/com/codersathi/userservice/User.java
@@ -1,13 +1,21 @@
 package com.codersathi.userservice;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String address;
+	private String username;
 
+	
 	public Long getId() {
 		return id;
 	}
@@ -22,6 +30,14 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 }
--- src/main/java/com/codersathi/userservice/UserController.java
+++ src/main/java/com/codersathi/userservice/UserController.java
@@ -1,8 +1,12 @@
 package com.codersathi.userservice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,24 +28,33 @@ public class UserController {
 		 return "Success!";
 	}
 
-	@GetMapping
-	public User getUser() {
-		return getStaticUser();
+	@GetMapping("/{userId}")
+	public User getUser(@PathVariable("userId") Long id) {
+		System.out.println("User ID "+ id);
+		return userService.getUser(id);
 	}
-
+	
+	@GetMapping
+	public List<User> getAllUser() {
+		return userService.getAllUser();
+	}
+	
 	@PutMapping
 	public User updateUser(@RequestBody User user) {
-		System.out.println("Update method called.");
-		User originalUser = getUser();
-		originalUser.setName(user.getName());
-		originalUser.setId(2L);
-		System.out.println("User Update Success!");
-		return originalUser;
+		
+		return userService.updateUser(user);
 	}
 
-//	@DeleteMapping
-	// TODOD
+	@DeleteMapping("/{userId}")
+	public void deleteUser(@PathVariable("userId") Long id) {
+		userService.deleteUser(id);
+	}
 
+	@DeleteMapping
+	public void deleteUser() {
+		userService.deleteUser();;
+	}
+	
 	private User getStaticUser() {
 		User user = new User();
 		user.setId(1L);
